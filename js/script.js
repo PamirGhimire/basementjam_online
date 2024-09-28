32cb51d61a91151657c6322a4b95bdc1527cf2e3
PamirGhimire <pamirghimire@gmail.com>
2024-09-28 17:12:19 +0200

    support homepage text in german and english

--- js/script.js
+++ js/script.js
@@ -2,3 +2,20 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("Website is fully loaded and ready!");
 });
+
+
+function toggleLanguageOnHomepage() {
+    var deutsch_text = document.getElementById("Intro_text_Deutsch");
+    var english_text = document.getElementById("Intro_text_English");
+    var button = event.target;
+
+    if (deutsch_text.style.display === "none") {
+        deutsch_text.style.display = "block";
+        english_text.style.display = "none";
+        button.innerHTML = "English";
+    } else {
+        deutsch_text.style.display = "none";
+        english_text.style.display = "block";
+        button.innerHTML = "Deutsch";
+    }
+}
